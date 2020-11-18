b96dfa06cb6bcedccf63bfd40b2aca01a4a798b5
Breno Xavier <brenoxav@gmail.com>
2020-11-18 17:51:37 -0400

    Refactor 'my_map', 'my_inject' and 'multiply_els'

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,3 @@
-# rubocop:disable Style/For
-
 module Enumerable
   def my_each
     block_given? ? size.times { |i| yield(to_a[i]) } : to_enum
@@ -22,73 +20,75 @@ module Enumerable
     if block_given? || arg.nil?
       my_each { |item| result = false unless yield item }
     elsif arg.is_a? Regexp
-      my_each { |item|  result = false unless item.to_s.match(arg) }
-    else arg.instance_of? Class
-      my_each { |item|  result = false unless item.is_a? == arg}
+      my_each { |item| result = false unless item.to_s.match(arg) }
+    elsif arg.instance_of? Class
+      my_each { |item| result = false unless item.is_a? == arg }
     end
     result
-    end
+  end
 
   def my_any?(arg = nil)
     result = false
     if block_given? || arg.nil?
       my_each { |item| result = true if yield item }
     elsif arg.is_a? Regexp
-      my_each { |item|  result = true if item.to_s.match(arg) }
-    else arg.instance_of? Class
-      my_each { |item|  result = true if item.is_a? == arg}
+      my_each { |item| result = true if item.to_s.match(arg) }
+    elsif arg.instance_of? Class
+      my_each { |item| result = true if item.is_a? == arg }
     end
     result
-    end
-
+  end
 
   def my_none?(arg = nil)
     result = true
     if block_given? || arg.nil?
       my_each { |item| result = false if yield item }
     elsif arg.is_a? Regexp
-      my_each { |item|  result = false if item.to_s.match(arg) }
-    else arg.instance_of? Class
-      my_each { |item|  result = false if item.is_a? == arg}
+      my_each { |item| result = false if item.to_s.match(arg) }
+    elsif arg.instance_of? Class
+      my_each { |item| result = false if item.is_a? == arg }
     end
     result
-    end
+  end
 
   def my_count(arg = nil)
     if arg.nil? && !block_given?
-      self.length
+      length
     else
-    counter = 0
-    my_each do |item| 
-      counter += 1 if yield item
+      counter = 0
+      my_each do |item|
+        counter += 1 if yield item
+      end
     end
-  end
     counter
   end
 
-  def my_map
-    if block_given?
-    finalarray = []
-    my_each do |item|
-      finalarray.push(yield item)
+  def my_map(proc = nil)
+    arr = []
+    return to_enum unless block_given?
+
+    if proc
+      my_each { |e| arr.push(proc.call(e)) }
+    else
+      my_each { |e| arr.push(yield(e)) }
     end
-    finalarray
-  else
-    to_enum(:my_map)
-  end
+    arr
   end
 
-  def my_inject
-    result = first
-    my_each_with_index do |item, index|
-      result = yield(result, item) if index.positive?
+  def my_inject(acc = nil)
+    my_each do |e|
+      if !acc
+        acc = e
+      elsif acc.instance_of?(Range)
+        my_each { |e| acc += e }
+      else
+        acc = yield(acc, e)
+      end
     end
-    result
+    acc
   end
 
   def multiply_els(arr)
-    arr.my_inject { |result, item| result * item }
+    arr.my_inject { |i, j| i * j }
   end
 end
-
-# rubocop:enable Style/For
