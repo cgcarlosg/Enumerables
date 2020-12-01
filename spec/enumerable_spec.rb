eb05eaa203ca6866689e0efbad9154564d37a347
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-01 12:38:15 -0500

    init rspec and add the tests in blank

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,68 @@
+require_relative '../enumerables'
+
+describe Enumerable do
+    let(:array) { [1, 2, 3] }
+    let(:hash) { { 'a' => 'enum', 'b' => 'module', 'c' => 'spec' } }
+    let(:range) { (1..3) }
+    let(:string) { ["one", "two", "three"] }
+
+describe '#my_each' do
+it  do
+    expect
+end
+end
+
+describe '#my_each_with_index' do
+it  do
+    expect
+end
+end
+
+describe 'my_select' do
+    it  do
+        expect
+    end
+end
+
+describe '#my_all?' do
+it  do
+    expect
+end
+end
+
+describe '#my_any?' do
+it  do
+    expect
+end
+end
+
+describe '#my_none?' do
+it  do
+    expect
+end
+end
+
+describe '#my_count' do
+it  do
+    expect
+end
+end
+
+describe '#my_map' do
+it  do
+    expect
+end
+end
+
+describe '#my_inject' do
+it  do
+    expect
+end
+end
+
+describe '#multiply_els' do
+it  do
+    expect
+end
+end
+end
